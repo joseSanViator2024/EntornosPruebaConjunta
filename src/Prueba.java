90824ea5e531f127b960d35423afee88b0315431
Christian987654321 <christian.quinone@sanviatorvalladolid.com>
2024-04-18 10:37:17 +0200

    añado linea en main

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Christian");
+		System.out.println("esta linea solo esta en rama Cristian, pero de momento no en master");
 
 		
 	}
